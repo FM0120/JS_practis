2d93a76acff057cc27fe36617fbef7617ee4261e
FM0120 <kaz.fumiaki@gmail.com>
2021-12-05 21:54:18 +0900

    update

--- JS_try/sample.js
+++ JS_try/sample.js
@@ -174,3 +174,128 @@
 
 //   return js_hand;
 // }
+
+// じゃんけんの手を入力してもらうプロンプト欄を生成
+// let user_hand = prompt('じゃんけんの手をグー、チョキ、パーから選んでください。');
+
+// // じゃんけんの手をランダムに作成する関数を呼び出す
+// let js_hand = getJShand();
+
+// // ユーザの手とJavaScriptのじゃんけんの手を比べる関数を呼び出し、結果をjudgeに入れる
+// let judge = winLose(user_hand, js_hand);
+
+// // 結果を表示する
+// alert('あなたの選んだ手は' + user_hand + 'です。\nJavaScriptの選んだ手は' + js_hand + 'です。\n結果は' + judge + 'です。');
+
+// // ランダムでじゃんけんの手を作成する関数
+// function getJShand(){
+//   let js_hand_num = Math.floor(Math.random()*3)
+//   let hand_name
+
+//   if(js_hand_num == 0){
+//     hand_name = "グー"
+//   } else if(js_hand_num == 1){
+//     hand_name = "チョキ"
+//   } else if(js_hand_num == 2){
+//     hand_name = "パー"
+//   }
+
+//   return hand_name;
+// }
+// // ユーザの手とJavaScriptのじゃんけんの手を比べる関数
+// function winLose(user,js){
+//   let winLoseStr
+//   if(user == "グー"){
+//     if(js == "グー"){
+//       winLoseStr = "あいこ";
+//     } else if(js == "チョキ"){
+//       winLoseStr = "勝ち";
+//     } else if(js == "パー"){
+//       winLoseStr = "負け";
+//     }
+//   } else if(user == "チョキ"){
+//     if(js == "グー"){
+//       winLoseStr = "負け";
+//     } else if(js == "チョキ"){
+//       winLoseStr = "あいこ";
+//     } else if(js == "パー"){
+//       winLoseStr = "勝ち";
+//     }
+//   } else if(user == "パー"){
+//     if(js == "グー"){
+//       winLoseStr = "勝ち";
+//     } else if(js == "チョキ"){
+//       winLoseStr = "負け";
+//     } else if(js == "パー"){
+//       winLoseStr = "あいこ";
+//     }
+//   }
+//   return winLoseStr;
+// }
+
+// promptとは、ユーザーの入力を受け付けるためのダイアログを表示するために使います。 ダイアログに入力した内容を表示することや、入力内容によって処理を分岐させることができます。 
+// また、ダイアログには初期値を設定することもできます。
+
+// while (条件式) 実行する文; for 文が指定した回数だけ繰り返す場合に主に利用されるのに対して、
+// while 文は指定した条件が満たされなくなるまで回数に制限なく繰り返しを行う場合に主に利用されます。
+// また for 文と異なり初期化式や変化式といったものがありません。
+// &&=左辺も右辺もtrueの場合にtrue（左辺 かつ 右辺）
+// !=右の値と左の値が違うか比べるときに使う
+// let user_hand = prompt('じゃんけんの手をグー、チョキ、パーから選んでください。')
+// while ((user_hand != "グー") && (user_hand != "チョキ") && (user_hand != "パー") && (user_hand != null)){
+// // &&=左辺も右辺もtrueの場合にtrue（左辺 かつ 右辺）
+// // !=右の値と左の値が違うか比べるときに使う
+//   alert('グー・チョキ・パーのいずれかを入力して下さい')
+//   user_hand = prompt('じゃんけんの手をグー、チョキ、パーから選んでください。')
+// }
+// let js_hand = getJShand()
+// let judge = winLose(user_hand, js_hand)
+
+// if (user_hand != null){
+//   alert('あなたの選んだ手は' + user_hand + 'です。\nJavaScriptの選んだ手は' + js_hand + 'です。\n結果は' + judge + 'です。')
+// } else {
+//   alert("またチャレンジしてね")
+// }
+
+// function getJShand(){
+//   let js_hand_num = Math.floor(Math.random() * 3)
+//   let hand;
+//   if (js_hand_num == 0){
+//     hand = "グー"
+//   } else if (js_hand_num == 1){
+//     hand = "チョキ"
+//   } else if (js_hand_num == 2){
+//     hand = "パー"
+//   }
+//   return hand;
+// }
+
+// function winLose(user, js){
+//   let winLoseStr;
+//   if (user == "グー"){
+//     if (js == "グー"){
+//       winLoseStr = "あいこ"
+//     } else if (js == "チョキ"){
+//       winLoseStr = "勝ち"
+//     } else if (js == "パー"){
+//       winLoseStr = "負け"
+//     }
+//   } else if (user == "チョキ"){
+//     if (js == "グー"){
+//       winLoseStr = "負け"
+//     } else if (js == "チョキ"){
+//       winLoseStr = "あいこ"
+//     } else if (js == "パー"){
+//       winLoseStr = "勝ち"
+//     }
+//   } else if (user == "パー"){
+//     if (js == "グー"){
+//       winLoseStr = "勝ち"
+//     } else if (js == "チョキ"){
+//       winLoseStr = "負け"
+//     } else if (js == "パー"){
+//       winLoseStr = "あいこ"
+//     }
+//   }
+//   return winLoseStr
+// }
